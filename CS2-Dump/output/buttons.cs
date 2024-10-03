1e25cdf64087c7d73e967c611e3f37641910877b
Nardss <111911668+Nardss@users.noreply.github.com>
2024-10-03 11:48:42 -0300

    Delete CS2-Dump directory

--- CS2-Dump/output/buttons.cs
+++
@@ -1,8 +0,0 @@
-// Generated using https://github.com/a2x/cs2-dumper
-// 2024-10-03 00:58:25.935762800 UTC
-
-namespace CS2Dumper {
-    // Module: client.dll
-    public static class Buttons {
-    }
-}
